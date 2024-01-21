8586969f32da601cd2f731ce0def636a2c924097
Tim Lochmüller <tim@fruit-lab.de>
2024-01-21 13:16:45 +0100

    Finish configuration

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ echo "------------ Update & Upgrade dependencies"
 sudo apt update && sudo apt-get -y upgrade
 
 echo "------------ Install dependencies"
-sudo apt-get install build-essential git-core cmake pkg-config subversion libcurl3-dev libsasl2-dev uuid-dev uuid-runtime libtool dh-autoreconf libunistring-dev git cmake pkg-config subversion libcurl4-openssl-dev libgnutls28-dev libsasl2-dev uuid-dev libtool libssl-dev libgcrypt20-dev libmicrohttpd-dev libltdl-dev libjson-c-dev libleptonica-dev libmosquitto-dev libunistring-dev dh-autoreconf
+sudo apt-get install build-essential git-core cmake libsml-dev pkg-config subversion libcurl3-dev libsasl2-dev uuid-dev uuid-runtime libtool dh-autoreconf libunistring-dev git cmake pkg-config subversion libcurl4-openssl-dev libgnutls28-dev libsasl2-dev uuid-dev libtool libssl-dev libgcrypt20-dev libmicrohttpd-dev libltdl-dev libjson-c-dev libleptonica-dev libmosquitto-dev libunistring-dev dh-autoreconf
 
 echo "------------ Install vzlogger to ~/vzlogger"
 cd ~
 git clone https://github.com/volkszaehler/vzlogger.git
 cd vzlogger
-echo "------------ Note: Confirm servic information with yes"
-./install.sh vzlogger
+echo "------------ Note: Confirm service information with yes"
+./install.sh vzlogger libsml
 
 echo "------------ Install resources-pi to ~/resources-pi"
 cd ~
@@ -28,4 +28,4 @@ systemctl status vzlogger
 
 # https://www.photovoltaikforum.com/thread/115770-vzlogger-und-direkte-http-abfrage-der-ausgelesenen-daten/
 
-echo "--------- done: Here is the data: http://resources-pi.local:8080/"
+echo "--------- done: Here is the data: http://resources-pi:8080/"
